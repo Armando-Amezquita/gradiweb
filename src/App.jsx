1beddacebf31ed0d1997142b1aeea5a6bd5a5cf7
Armando Amezquita <a.amezquita.1009@gmail.com>
2023-03-12 20:39:18 -0500

    fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -9,44 +9,38 @@ function App() {
   const [quantity, setQuantity] = useState(0)
   const [discover, setDiscover] = useState('')
 
-  const getDataTotalProductsSell = async() => {
-    console.log('prpducttoz')
-    const data = await fetch(`${URLPRODUCTSELL}`)
+  async function getDataTotalProductsSell() {
+    new Promise((resolve, reject) => {
+      fetch(`${URLPRODUCTSELL}`)
         .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.log(err));
-        
-    const orders = data.orders.nodes;
-    let lineItemsTotal = 0;
+        .then(res => {
+          const orders = res.orders.nodes;
+          let lineItemsTotal = 0;
 
-    for (const order of orders) {
-        for (const lineitem of order.lineItems.nodes) {
-            lineItemsTotal += lineitem.quantity
-        }
-    }
-    return Math.floor(lineItemsTotal / orders.length); 
+          for (const order of orders) {
+            for (const lineitem of order.lineItems.nodes) {
+              lineItemsTotal += lineitem.quantity
+            }
+          }
+          setQuantity(Math.floor(lineItemsTotal / orders.length));
+        })
+    })
+
   }
-  const getDataTotalCarrousel = async() => {
-    try{
-        
-        const data = await fetch(`${URLDISCOVER}`)
-            .then(data => data.json())
-            .then(res => res)
-            .catch(err => console.log(err))
 
-        const products = data.products.nodes;
+  async function getDataTotalCarrousel() {
+    fetch(`${URLDISCOVER}`)
+      .then(data => data.json())
+      .then(res => {
+        setDiscover(res.products.nodes)
+      })
+  }
 
-        return products;
-
-    }catch(err){
-        console.log('err', err)
-    }
-}
-  useEffect( () => async() => {
-    console.log('userEffect')
-      setQuantity( await getDataTotalProductsSell());
-      setDiscover(await getDataTotalCarrousel())
+  useEffect(() => {
+    getDataTotalProductsSell()
+    getDataTotalCarrousel()
   }, [])
+  
 
   return (
     <div className="App">
